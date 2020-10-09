447eb0d977186c6478015bf9603a70e83ac0a2cd
Allan <alan92goncalves@gmail.com>
2020-10-09 19:01:00 -0300

    func getproductsbyid

---
+++ GetProductsById/index.js
@@ -0,0 +1,15 @@
+const createMongoCLient = require('../shared/mongoClient');
+const { ObjectID } = require('mongodb');
+
+module.exports = async function (context, req) {
+    const { id } = req.params;
+    const { client: MongClient, closeConnectionFn } = await createMongoCLient();
+    const Products = MongClient.collection('products');
+    const res = await Products.findOne({ _id: ObjectID(id) });
+   
+    closeConnectionFn();
+    context.res = {
+        status: 200,
+        body: res,
+    }
+}
